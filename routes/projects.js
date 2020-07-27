77419e6319857453c95cc45c7eba4a72324656d4
Keracy <fazzzle.de@gmail.com>
2020-07-27 18:25:02 +0300

    Add patch and delete requests for projects

--- routes/projects.js
+++ routes/projects.js
@@ -13,7 +13,7 @@ router.post("/", async (req, res) => {
     await project.save();
     return res.status(200).send(project);
   } catch (err) {
-    res.status(500).send("error");
+    res.status(500).send(err);
   }
 });
 
@@ -22,7 +22,34 @@ router.get("/", async (req, res) => {
     const projects = await Project.find();
     res.status(200).send(projects);
   } catch (err) {
-    res.status(500).send("error");
+    res.status(500).send(err);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const project = await Project.findByIdAndRemove(req.body._id);
+    res.status(200).send(project);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch("/", async (req, res) => {
+  const { title, status, devs, rate, _id } = req.body;
+  const project = {
+    _id: _id,
+    title: title,
+    status: status,
+    devs: devs,
+    rate: rate,
+  };
+  try {
+    await Project.findByIdAndUpdate(_id, project);
+    res.status(200).send(project);
+  } catch (err) {
+    res.status(500).send(err);
   }
 });
 
